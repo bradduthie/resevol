776d45527d627f4c8d87df7c7e16f99456728cfe
Brad Duthie <brad.duthie@gmail.com>
2018-08-13 11:22:26 +0100

    Two new functions

--- R/toy_model.R
+++ R/toy_model.R
@@ -1,6 +1,11 @@
 # ==============================================================================
 # This is just a toy version of the model, for the presentation
 # ==============================================================================
+# Note that I'm adding 'toy' at the start of each function; this is just in case
+# we actually want to keep some of these functions in the eventual R package, 
+# but also don't want to worry about not being able to name functions like
+# "initialise_land" the same here as elsewhere in the eventual bigger model.
+# ==============================================================================
 
 # Initialise the landscape
 toy_initialise_land <- function(xdim = 2, ydim = 2, pathogens = 1, crops = 1){
@@ -9,7 +14,49 @@ toy_initialise_land <- function(xdim = 2, ydim = 2, pathogens = 1, crops = 1){
     p_layer   <- matrix(data = p_values, ncol = xdim, nrow = ydim);
     c_values  <- sample(x = 1:crops, size = xdim * ydim, replace = TRUE);
     c_layer   <- matrix(data = c_values, ncol = xdim, nrow = ydim);
-    LAND[,,2] <- p_layer;
-    LAND[,,3] <- c_layer;
+    LAND[,,2] <- p_layer; # Just occurred to me that we might want a layer for
+    LAND[,,3] <- c_layer; # carrying capacity of the cell in LAND later
     return(LAND);
 }
+
+# Initialise some *very* simple pests. Each allele is just going to map one to
+# one for whether a crop can be attacked or a pathogen resisted. I am not even
+# going to add traits affecting dispersal in the toy version -- this can be
+# uniform for all individuals
+toy_initialise_pest <- function(LAND, N = 10, p_al = 1, c_al = 1){
+    xdim     <- dim(LAND)[1]; # This is needed to put the pests on some
+    ydim     <- dim(LAND)[2]; # landscape cell
+    PEST     <- matrix(data = 0, nrow = N, ncol = 8);
+    PEST[,1] <- 1:N;                                           # ID
+    PEST[,2] <- sample(x = c(0, 1), size = N, replace = TRUE); # Sex
+    PEST[,3] <- sample(x = 1:xdim,  size = N, replace = TRUE); # x-location
+    PEST[,4] <- sample(x = 1:ydim,  size = N, replace = TRUE); # y-location
+    PEST[,5] <- sample(x = 1:p_al,  size = N, replace = TRUE); # p allele 1
+    PEST[,6] <- sample(x = 1:p_al,  size = N, replace = TRUE); # p allele 2
+    PEST[,7] <- sample(x = 1:c_al,  size = N, replace = TRUE); # c allele 1
+    PEST[,8] <- sample(x = 1:c_al,  size = N, replace = TRUE); # c allele 2
+    return(PEST);
+}
+
+# Just going to make this a simple function at first, randomly changing crops
+toy_set_crops <- function(LAND, crops = 1, type = "rotate"){
+    if(crops == 1){
+        return(LAND);
+    }
+    if(type == "rotate"){
+        old_crop                   <- LAND[,,3];
+        new_crop                   <- old_crop + 1;
+        new_crop[new_crop > crops] <- 1;
+        LAND[,,3]                  <- new_crop;
+    }else{ # Else it just randomises -- can think about fancier ways later
+        xdim      <- dim(LAND)[1];
+        ydim      <- dim(LAND)[2];
+        new_cval  <- sample(x = 1:crops, size = xdim * ydim, replace = TRUE);
+        new_crop  <- matrix(data = new_cval, nrow = xdim, ncol = ydim);
+        LAND[,,3] <- new_crop;
+    }
+    return(LAND);
+}
+
+
+
