5f1177261c42554e58319a8e70d74aaf66754ab5
Brad Duthie <brad.duthie@gmail.com>
2021-05-29 17:51:12 +0100

    Get the main parts initialised for individuals; can now actually put them down on a landscape

--- R/initialise.R
+++ R/initialise.R
@@ -14,20 +14,34 @@ initialise_inds <- function(mine_output, N = 1000, xdim = 100, ydim = 100){
     layers     <- mine_output[[1]][2];
     traits     <- dim(mine_output[[2]])[1];
     
-    ind_Lv     <- rnorm(n = N * loci, mean = 0, sd = 1);
-    ind_Lm     <- matrix(data = ind_Lv, nrow = N, ncol = loci);
-    ind_Tr     <- ind_Lm %*% mine_output[[5]];
+    ind_loci_vals   <- rnorm(n = N * loci, mean = 0, sd = 1);
+    ind_loci_mat    <- matrix(data = ind_loci_vals, nrow = N, ncol = loci);
+    ind_traits_mat  <- ind_loci_mat %*% mine_output[[5]];
+    genome          <- mine_output[[7]];
+    ind_first_cols  <- matrix(data = 0, nrow = N, ncol = 20);
     
-    ind_ini    <- matrix(data = 0, nrow = N, ncol = 20);
+    trait_start_col   <- dim(ind_first_cols)[2] + 1;
+    layers_start_col  <- trait_start_col + traits;
+    loci_start_col    <- layers_start_col + layers + 1;
+    genome_start_col  <- loci_start_col + loci;
+    genome_end_col    <- genome_start_col + length(genome) - 1;
+  
+    net_start_col     <- genome_start_col + (loci * traits);
+    net_layer_sep     <- seq(from = net_start_col, to = genome_end_col, 
+                             by = (traits * traits));
+    net_separators    <- c(genome_start_col, net_layer_sep);
     
-    trait_st   <- 21;
-    loci_st    <- trait_st + traits;
-    layers_st  <- loci_st + loci;
-    genome_st  <- layers_st + layers;
-    genome_end <- genome_st + length(mine_output[[7]]);
+    net_layer_cols    <- matrix(data = net_separators, nrow = N, 
+                                ncol = length(net_separators), byrow = TRUE);
+    ind_genome_cols   <- matrix(data = genome, nrow = N, ncol = length(genome),
+                                byrow = TRUE);
     
-    net_st     <- genome_st + (loci * traits);
-    nets       <- seq(from = net_st, to = genome_end, by = (traits * traits));
+    inds       <- matrix(data = 0, nrow = N, ncol = genome_end_col);
+
+    inds[, trait_start_col:(layers_start_col - 1)]  <- ind_traits_mat;
+    inds[, layers_start_col:(loci_start_col - 1)]   <- net_layer_cols;
+    inds[, loci_start_col:(genome_start_col - 1)]   <- ind_loci_mat;
+    inds[, genome_start_col:genome_end_col]         <- ind_genome_cols;
     
     # Going to bind as follows:
     # 1. 20 columns that are empty (starts at 1)
