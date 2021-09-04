de5f05f91bdfc7d813e02d8f526cdcf4816bdf92
Brad Duthie <brad.duthie@gmail.com>
2021-09-04 22:30:41 +0100

    Introduce metabolism into the aging function, which burns some of the consumed food

--- src/age.c
+++ src/age.c
@@ -7,12 +7,20 @@
  * ========================================================================== */
 void age_pests(double **pests, double *paras){
   
-  int ind, N, age_col;
+  int ind, N, age_col, meta_col, cons_col;
   
-  age_col = (int) paras[3];
-  N       = (int) paras[101];
+  age_col  = (int) paras[3];
+  N        = (int) paras[101];
+  meta_col = (int) paras[86];
+  cons_col = (int) paras[14];
 
   for(ind = 0; ind < N; ind++){
+    if(pests[ind][age_col] > 0){
+        pests[ind][cons_col] -= pests[ind][meta_col];
+        if(pests[ind][cons_col] < 0){
+            pests[ind][cons_col] = 0;
+        }
+    }
     pests[ind][age_col]++;
   }
   
