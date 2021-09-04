9ee08c3bf98ae2a5298ca671ae0736b2de5770c6
Brad Duthie <brad.duthie@gmail.com>
2021-09-04 22:40:28 +0100

    Just get the floored metabolic rate done so that this does not come back to be a problem for Issue #38 -- I will still want to do the baseline rate tomorrow

--- src/age.c
+++ src/age.c
@@ -8,6 +8,7 @@
 void age_pests(double **pests, double *paras){
   
   int ind, N, age_col, meta_col, cons_col;
+  double meta_rate;
   
   age_col  = (int) paras[3];
   N        = (int) paras[101];
@@ -15,8 +16,12 @@ void age_pests(double **pests, double *paras){
   cons_col = (int) paras[14];
 
   for(ind = 0; ind < N; ind++){
+    meta_rate = pests[ind][meta_col];
+    if(meta_rate < 0){
+      meta_rate = 0.0;
+    }
     if(pests[ind][age_col] > 0){
-        pests[ind][cons_col] -= pests[ind][meta_col];
+        pests[ind][cons_col] -= meta_rate;
         if(pests[ind][cons_col] < 0){
             pests[ind][cons_col] = 0;
         }
