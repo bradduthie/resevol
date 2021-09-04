63af98055a4c144baee8cfd728f31a5ee0cca1bc
Brad Duthie <brad.duthie@gmail.com>
2021-09-04 17:31:57 +0100

    Add some mine_gmatrix outputs to the R package to be used as examples in the vignette and by users

---
+++ R/gmatrices.R
@@ -0,0 +1,96 @@
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure of the identity matrix (replicate 1)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_n1"
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure of the identity matrix (replicate 2)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_n2"
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure of the identity matrix (replicate 3)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_n3"
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure  of the identity matrix (replicate 4)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_n4"
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure of the identity matrix (replicate 5)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_n5"
+
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure including negative and positive values 
+#' (replicate 1)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_v1"
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure including negative and positive values 
+#' (replicate 2)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_v2"
+
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure including negative and positive values 
+#' (replicate 3)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_v3"
+
+
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure including negative and positive values 
+#' (replicate 4)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_v4"
+
+
+#' Sample mine_gmatrix outputs
+#'
+#' Output from mine_gmatrix that maps random normals from 12 loci to four traits
+#' with a covariance structure including negative and positive values 
+#' (replicate 5)
+#'
+#' @format A list of 8 elements used in individual-based model initialisation
+"mg_v5"
+
+
+
+
+
+
+
